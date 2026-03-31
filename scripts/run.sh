12b1272d3e934b433e41971338c1d93fef6a563a
Kevin Alavik <kevin@alavik.se>
2026-03-31 22:27:06 +0200

    feat: tried toget init working

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+LIVECD=$1
+QEMU_FLAGS="-m 4G -smp 4 -serial stdio"
+QEMU_MACHINE_FLAGS="-machine q35"
+
+qemu-system-x86_64 $QEMU_FLAGS $QEMU_MACHINE_FLAGS \
+	-drive if=pflash,format=raw,unit=0,file=test/ovmf_code-x86_64.fd,readonly=on \
+	-drive if=pflash,format=raw,unit=1,file=test/ovmf_vars-x86_64.fd \
+	-device uefi-vars-x64,jsonfile=test/nvram.json \
+	-cdrom $LIVECD -d guest_errors
